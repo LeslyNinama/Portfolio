b51b8abdf1cbd38c0621610d1d046351e9297913
Saitnos <leslyshreyashninama19@gmail.com>
2024-05-27 23:36:15 +0530

    Initial Commit

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg", width=350)
+    st.image("images/Photo.jpg", width=350)
 
 with col2:
     st.title("Lesly Shreyash Ninama")
