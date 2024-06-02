f174269bf818ecf216e1c2f2136eda02f2674244
Saitnos <leslyshreyashninama19@gmail.com>
2024-06-02 18:42:18 +0530

    Securing Password Using Environment Variable

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
     host = 'smtp.gmail.com'
     port = 465
 
     username = 'leslyshreyashninama19@gmail.com'
-    password = 'tusjodystcdmdfev'
+    password = os.getenv('PASSWORD')
 
     receiver = 'leslyshreyashninama19@gmail.com'
     context = ssl.create_default_context()
